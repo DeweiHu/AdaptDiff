45e3871b245830b6457f71aa8da7f69efa573bcf
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2023-10-24 10:24:41 -0500

    Add files via upload

--- src/modules.py
+++ src/modules.py
@@ -51,10 +51,10 @@ class SPADE_residual_block(nn.Module):
         hidden_nch = min(input_nch, output_nch)
 
         self.conv_1 = nn.Conv2d(input_nch, hidden_nch, kernel_size=3, padding=1)
-        self.SPADE_norm_1 = SPADE(seg_nch, hidden_nch)
+        self.SPADE_norm_1 = SPADE(seg_nch, input_nch)
         
         self.conv_2 = nn.Conv2d(hidden_nch, output_nch, kernel_size=3, padding=1)
-        self.SPADE_norm_2 = SPADE(hidden_nch, output_nch)
+        self.SPADE_norm_2 = SPADE(seg_nch, hidden_nch)
 
         self.activate = nn.GELU()
 
@@ -67,6 +67,109 @@ class SPADE_residual_block(nn.Module):
         return output
 
 
+class residual_block(nn.Module):
+
+    def __init__(self, nch_in, nch_out):
+        super(residual_block,self).__init__()
+        self.align = nn.Conv2d(in_channels = nch_in,
+                               out_channels = nch_out,
+                               kernel_size = 1,
+                               stride = 1,
+                               padding = 0)
+        self.dualconv = nn.Sequential(
+                nn.Conv2d(in_channels = nch_out,
+                          out_channels = nch_out,
+                          kernel_size = 3,
+                          stride = 1,
+                          padding = 1),
+                nn.BatchNorm2d(num_features = nch_out),
+                nn.GELU(),
+                nn.Conv2d(in_channels = nch_out,
+                          out_channels = nch_out,
+                          kernel_size = 3,
+                          stride = 1,
+                          padding = 1),
+                nn.BatchNorm2d(num_features = nch_out)
+                )
+        self.gelu = nn.GELU()
+    
+    def forward(self, x):
+        x = self.align(x)
+        x1 = self.dualconv(x)
+        opt = self.gelu(torch.add(x,x1))
+        return opt
+    
+    
+def trans_down(nch_in, nch_out):
+    return nn.Sequential(
+            nn.Conv2d(in_channels=nch_in, 
+                      out_channels=nch_out, 
+                      kernel_size=4,
+                      stride=2, 
+                      padding=1),
+            nn.BatchNorm2d(nch_out),
+            nn.GELU()
+            )
+            
+            
+def trans_up(nch_in,nch_out):
+    return nn.Sequential(
+            nn.ConvTranspose2d(in_channels=nch_in, 
+                               out_channels=nch_out,
+                               kernel_size=4, 
+                               stride=2, 
+                               padding=1),
+            nn.BatchNorm2d(nch_out),
+            nn.GELU()
+            )
+
+
+class res_UNet(nn.Module):
+    def __init__(self, nch_enc, nch_in, nch_out):
+        super(res_UNet,self).__init__()
+        
+        # (assume input_channel=1)
+        self.nch_in = nch_in
+        self.nch_enc = nch_enc
+        self.nch_aug = (self.nch_in,)+self.nch_enc
+        
+        # module list
+        self.encoder = nn.ModuleList()
+        self.decoder = nn.ModuleList()
+        self.td = nn.ModuleList()
+        self.tu = nn.ModuleList()
+        
+        for i in range(len(self.nch_enc)):
+            # encoder & downsample
+            self.encoder.append(residual_block(self.nch_aug[i], self.nch_aug[i+1]))
+            self.td.append(trans_down(self.nch_enc[i], self.nch_enc[i]))
+            # decoder & upsample
+            self.tu.append(trans_up(self.nch_enc[-1-i], self.nch_enc[-1-i]))
+            if i == len(self.nch_enc)-1:
+                self.decoder.append(residual_block(self.nch_aug[-1-i]*2, nch_out))
+            else:
+                self.decoder.append(residual_block(self.nch_aug[-1-i]*2, self.nch_aug[-2-i]))
+    
+    
+    def forward(self, x):
+        cats = []
+        # encoder
+        for i in range(len(self.nch_enc)):
+            layer_opt = self.encoder[i](x)
+            x = self.td[i](layer_opt)
+            cats.append(layer_opt)
+        
+        # bottom layer
+        layer_opt = x
+        
+        # decoder
+        for i in range(len(self.nch_enc)):
+            x = self.tu[i](layer_opt)
+            x = torch.cat([x,cats[-1-i]],dim=1)
+            layer_opt = self.decoder[i](x)
+
+        y_pred = layer_opt
+        return y_pred
 
 
 if __name__ == "__main__":
