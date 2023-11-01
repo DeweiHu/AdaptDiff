12e0ca657974ace1e384152c6ce3e579f3ec03ca
Dewei Hu <50162880+DeweiHu@users.noreply.github.com>
2023-10-31 21:35:19 -0500

    Add files via upload

---
+++ src/dataloader.py
@@ -0,0 +1,28 @@
+import util
+import numpy as np
+import imageio
+import os
+from PIL import Image
+
+data_path = "/media/dewei/New Volume/data/"
+
+# fundus data 
+FP_data = {}
+FP_data_path = {}
+
+FP_data_path["drive"] = data_path + "DRIVE/training/"
+FP_data_path["stare"] = data_path + "STARE/"
+FP_data_path["drive"] = data_path + "DRIVE/training/"
+FP_data_path["drive"] = data_path + "DRIVE/training/"
+FP_data_path["drive"] = data_path + "DRIVE/training/"
+FP_data_path["drive"] = data_path + "DRIVE/training/"
+
+
+
+
+#%% OCTA data
+OCTA_data = {}
+
+#%% FA data
+FA_data = {}
+
